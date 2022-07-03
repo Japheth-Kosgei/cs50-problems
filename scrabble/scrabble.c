c19b6327d492aa430d05be36189563cbd1cea64d
JaphethK <74491890+Japheth-prog@users.noreply.github.com>
2022-07-03 07:54:08 +0000

    Sun, Jul 3, 2022, 7:54 AM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -19,6 +19,20 @@ int main(void)
     int score2 = compute_score(word2);
 
     // TODO: Print the winner
+    if (score1 == score2)
+    {
+        printf("Tie!");
+    }
+
+    else if (score1 > score2)
+    {
+        printf("Player 1 wins!");
+    }
+    
+    else
+    {
+        printf("Player 2 wins!");
+    }
 }
 
 int compute_score(string word)
