8ae7e92b43185650455d634edd1b55d67542fcb6
JaphethK <74491890+Japheth-prog@users.noreply.github.com>
2022-07-03 10:47:15 +0000

    Sun, Jul 3, 2022, 10:47 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int count_letters(string text);
 
@@ -7,9 +8,10 @@ int main(void)
 {
     // Prompt the user for text
     string text = get_string("Text: ");
-    printf("%s\n", text);
 
     // Count the number of letters
+    int number_letters = count_letters(text);
+    printf("%i\n", number_letters);
 
     // Count the number of words
 
@@ -19,5 +21,11 @@ int main(void)
 
 int count_letters(string text)
 {
-    
+    int length_of_string = strlen(text);
+    int number_letters = 0;
+
+    for (int i = 0; i < number_letters; i++)
+    {
+        
+    }
 }
