1b5d6a7d6401a8d4800804a159a5f9955ac46698
JaphethK <74491890+Japheth-prog@users.noreply.github.com>
2022-07-03 10:36:08 +0000

    Sun, Jul 3, 2022, 10:36 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,7 +1,15 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
+    // Prompt the user for text
+    string text = get_string("Text: \n ");
 
+    // Count the number of letters
+
+    // Count the number of words
+
+    // Count the number of sentences
+    
 }
