8a972c360c7cbf09bc3b3fb6448ca07e376b6357
JaphethK <74491890+Japheth-prog@users.noreply.github.com>
2022-07-03 11:06:57 +0000

    Sun, Jul 3, 2022, 11:06 AM +00:00

---
+++ readability/letter.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(int argc, string argv[])
+{
+    // Take in a commandline argument and print out is len
+    string text = argv[];
+    int len = strlen(text);
+
+    printf("%i\n", len);
+}
