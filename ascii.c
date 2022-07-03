f6f967ddcb2f33522fc678e58da316372ae955b8
JaphethK <74491890+Japheth-prog@users.noreply.github.com>
2022-07-03 09:43:25 +0000

    Sun, Jul 3, 2022, 9:43 AM +00:00

--- ascii.c
+++ ascii.c
@@ -4,5 +4,6 @@
 int main(void)
 {
     string name = get_string("Enter a character: ");
-    printf("The ASCII value is %d\n", name[0]);
+    int ascii_value = name[0];
+    printf("The ASCII value is %i\n", ascii_value);
 }
