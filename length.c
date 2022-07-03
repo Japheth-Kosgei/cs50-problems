402478d950966e502c631acb0a807fb479107278
JaphethK <74491890+Japheth-prog@users.noreply.github.com>
2022-07-03 08:18:57 +0000

    Sun, Jul 3, 2022, 8:18 AM +00:00

--- length.c
+++ length.c
@@ -3,5 +3,11 @@
 
 int main()
 {
-    // In case you forget,
+    // In case you forget, find the length of a string(array of characters)
+    // Get string
+    string name = get_string("Enter your name: ");
+
+    // Find the number of characters in the string
+    int lenght = 0;
+    for (int i=0; )
 }
