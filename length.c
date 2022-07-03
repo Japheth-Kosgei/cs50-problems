780ea70c552b695447659ba55acbcda11cd77e73
JaphethK <74491890+Japheth-prog@users.noreply.github.com>
2022-07-03 08:27:09 +0000

    Sun, Jul 3, 2022, 8:27 AM +00:00

--- length.c
+++ length.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <cs50.h>
 
+int get_string_length(string name);
+
 int main()
 {
     // In case you forget, find the length of a string(array of characters)
     // Get string
-    string name = get_string("Enter your name: ");
+    string name_string = get_string("Enter your name: ");
 
     // Find the number of characters in the string
-    int length = 0;
-    while (name[length] != 0)
-    {
-        length ++;
-    }
-    printf("%i\n", length);
+    int len = get_string(name_string);
+}
+
+int get_string_length(string name)
+{
+    int
 }
