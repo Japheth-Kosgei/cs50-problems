758f108c59648d60fdda35719db824826df819c9
JaphethK <74491890+Japheth-prog@users.noreply.github.com>
2022-07-03 10:37:11 +0000

    Sun, Jul 3, 2022, 10:37 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -5,11 +5,12 @@ int main(void)
 {
     // Prompt the user for text
     string text = get_string("Text: \n ");
+    printdf("%s\n", text);
 
     // Count the number of letters
 
     // Count the number of words
 
     // Count the number of sentences
-    
+
 }
