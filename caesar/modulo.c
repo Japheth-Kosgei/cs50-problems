467e8bc399ba876e2da6bfc3fd972c41807a0dbe
JaphethK <74491890+Japheth-prog@users.noreply.github.com>
2022-07-04 14:36:37 +0000

    Mon, Jul 4, 2022, 2:36 PM +00:00

--- caesar/modulo.c
+++ caesar/modulo.c
@@ -9,7 +9,7 @@ int main(void)
     // Find out what modulo does
     int plain_text_char = get_int("plain_text_char: ");
     int key = get_int("key:  ");
-    int answer = (plain_text_char + key) % 26;
+    int answer = (plain_text_char + key);
 
     printf("Answer is %i\n", answer);
 }
