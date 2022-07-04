daa746ac642b530fc9b4ef214d27fa4217bac694
JaphethK <74491890+Japheth-prog@users.noreply.github.com>
2022-07-04 10:26:46 +0000

    Mon, Jul 4, 2022, 10:26 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -8,6 +8,13 @@ int main(int argc, string argv[])
             // If it doesn't have a commandline argument
             // It has more then one command line argument
             // It is not a decimal digit; and print an error meassage
+
     // Do the encryption
+        // For each character in the plaintext
+            // If it is a lower case character;
+                // Rotate using k
+            // If it is upper case
+                // Rotate using but differently
+
     // Print the ciphertext
 }
