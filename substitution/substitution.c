e3956d12a8234cc00e87e9054b3c8f11ee2f7ead
JaphethK <74491890+Japheth-prog@users.noreply.github.com>
2022-07-04 15:34:09 +0000

    Mon, Jul 4, 2022, 3:34 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,21 +1,40 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 int main(int argc, string argv[])
 {
     // Check if the arguement count is one, not less, not more
     if (argc < 2 || argc > 2)
     {
-        printf("Usage: ./substituion KEY");
+        printf("Usage: ./substituion KEY\n");
         return 1;
     }
 
-    // Check if key is valid: 26 letters, each letter should appear only once.
+    // Check if key is valid: not containing non-alhpabetic letters, 26 letters, each letter should appear only once.
     string key = argv[1];
     int key_length = strlen(key);
 
-    if key_length
+    // Check if key has 26 letters
+    if (key_length != 26)
+    {
+        printf("Key must have 26 characters.\n");
+        return 1;
+    }
+
+    // Check if all characters are alphabetical
+    for (int i = 0; i < key_length; i ++)
+    {
+        if (!isalpha(key[i]))
+        {
+            printf("Key must contain alphabetical characters only.\n");
+            return 1;
+        }
+    }
+
+
+
     // Change the key to all-uppercase
     // Print plaintext
     // Print ciphertext
