e442aa7d35b44c97cdfbd36536595696e30a45ac
JaphethK <74491890+Japheth-prog@users.noreply.github.com>
2022-07-04 10:23:12 +0000

    Mon, Jul 4, 2022, 10:23 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -4,7 +4,10 @@
 int main(int argc, string argv[])
 {
     // Accept input
-        // If it doesn't have a
+        // Return 1 if:
+            // If it doesn't have a commandline argument
+            // It has more then one command line argument
+            // It is not a decimal digit; and print an error meassage
     // Do the encryption
     // Print the ciphertext
 }
