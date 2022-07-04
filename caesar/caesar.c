7d4b8df39341e7dd5eec15b657eaa319d8442d16
JaphethK <74491890+Japheth-prog@users.noreply.github.com>
2022-07-04 10:17:21 +0000

    Mon, Jul 4, 2022, 10:17 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,7 +1,9 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
-
+    // Accept input
+    // Do the encryption
+    // Print the ciphertext
 }
