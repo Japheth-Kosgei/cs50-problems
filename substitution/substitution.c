a40585dfe0def68028e819088991032dd6d00281
JaphethK <74491890+Japheth-prog@users.noreply.github.com>
2022-07-04 15:24:18 +0000

    Mon, Jul 4, 2022, 3:24 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -4,7 +4,15 @@
 int main(int argc, string argv[])
 {
     // Check if the arguement count is one, not less, not more
+    if (argc < 2 || argc > 2)
+    {
+        printf("Usage: ./substituion KEY");
+        return 1;
+    }
+
     // Check if key is valid: 26 letters, each letter should appear only once.
+    string key = argv[1];
+    
     // Change the key to all-uppercase
     // Print plaintext
     // Print ciphertext
