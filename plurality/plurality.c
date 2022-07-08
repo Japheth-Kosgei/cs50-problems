32ac043ffb8fd715ca7d8a0c071755d3bc562af3
JaphethK <74491890+Japheth-prog@users.noreply.github.com>
2022-07-08 12:39:12 +0000

    Fri, Jul 8, 2022, 12:39 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -80,6 +80,9 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i ++)
+    {
+
+    }
     return;
 }
