2bac6a02fabde5fcb4d300aa1d5947da22f99b2c
JaphethK <74491890+Japheth-prog@users.noreply.github.com>
2022-07-08 12:21:00 +0000

    Fri, Jul 8, 2022, 12:21 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -66,7 +66,10 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
-    for (int i = 0; )
+    for (int i = 0; i < candidate_count; i ++)
+    {
+        
+    }
     return false;
 }
 
