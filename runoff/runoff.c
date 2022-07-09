51a02bd561c99d0d9b494aca53c7370229e6f683
JaphethK <74491890+Japheth-prog@users.noreply.github.com>
2022-07-09 17:47:47 +0000

    Sat, Jul 9, 2022, 5:47 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -165,10 +165,10 @@ void tabulate(void)
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
-    int winning_vote = 
+    int winning_vote = (voter_count / 2) + 1;
     for (int i = 0; i < candidate_count; i ++)
     {
-        if (candidates[i].votes > voter_count / 2)
+        if (candidates[i].votes > winning_vote)
         {
             printf("%s\n", candidates[i].name);
             return true;
