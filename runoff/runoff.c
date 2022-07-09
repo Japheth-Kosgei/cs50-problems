e13f47910cd4708e7f1b4578a822920c05622221
JaphethK <74491890+Japheth-prog@users.noreply.github.com>
2022-07-09 11:39:43 +0000

    Sat, Jul 9, 2022, 11:39 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -130,7 +130,7 @@ bool vote(int voter, int rank, string name)
 {
     for (int i = 0; i < candidate_count; i ++)
     {
-        if ()
+        if (strcmp(name, candidates[i].name))
     }
     return false;
 }
