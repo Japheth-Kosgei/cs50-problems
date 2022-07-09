db2522886a0317f095c377a4bf39cc8251ceb151
JaphethK <74491890+Japheth-prog@users.noreply.github.com>
2022-07-09 17:19:47 +0000

    Sat, Jul 9, 2022, 5:19 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -197,13 +197,25 @@ int find_min(void)
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
 {
-    
+    for (int i = 0; i < candidate_count; i ++)
+    {
+        if (candidates[i].votes == min)
+        {
+            return true;
+        }
+    }
     return false;
 }
 
 // Eliminate the candidate (or candidates) in last place
 void eliminate(int min)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i ++)
+    {
+        if (candidates[i].votes == min)
+        {
+            candidates[i].eliminated = true;
+        }
+    }
     return;
 }
