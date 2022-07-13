d5e91f9935092bae5aa1233f77cfdd5bb9a213f5
JaphethK <74491890+Japheth-prog@users.noreply.github.com>
2022-07-13 14:34:53 +0000

    Wed, Jul 13, 2022, 2:34 PM +00:00

--- fact.c
+++ fact.c
@@ -4,15 +4,17 @@ int fact(int n);
 
 int main(void)
 {
+    // Do a factorial
     printf("%i\n", fact(5));
 }
 
 int fact(int n)
 {
+    // Recursive function
     if (n == 1)
     {
-        return 1;
+        return 1;  // This is the end of the stack/loop, and it returns int 1; NOT an error.
     }
 
-    return n * (n-1);
+    return n * fact(n-1);  // This multiplies n by the return value of fact.
 }
