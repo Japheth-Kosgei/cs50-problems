058f0b5139cdd02cbbfaad9a178d9712780001f7
JaphethK <74491890+Japheth-prog@users.noreply.github.com>
2022-07-13 13:53:26 +0000

    Wed, Jul 13, 2022, 1:53 PM +00:00

---
+++ copy.c
@@ -0,0 +1,12 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void)
+{
+    // copy content from one variable to another
+    char x = get_string("x: ");
+    char y = x;
+
+    // Make it upper to know if it copied correctly
+}
