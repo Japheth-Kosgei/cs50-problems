96dab745ae7539b36eed7ccffb702e71394310ed
JaphethK <74491890+Japheth-prog@users.noreply.github.com>
2022-07-13 13:28:01 +0000

    Wed, Jul 13, 2022, 1:28 PM +00:00

--- address.c
+++ address.c
@@ -2,6 +2,8 @@
 
 int main(void)
 {
-    int n = 5;
-    printf("%p\n", &n);
+    int n = 5; // The content
+    int *pn = &n; // The pointer of int n
+    printf("%p\n", pn); // Print the pointer
+    printf("%i\n", *pn); // Print the value of the pointer.
 }
