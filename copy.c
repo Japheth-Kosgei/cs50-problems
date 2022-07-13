e56906b6d2a2b5ec34c524bb134bb9ad5047172d
JaphethK <74491890+Japheth-prog@users.noreply.github.com>
2022-07-13 13:59:49 +0000

    Wed, Jul 13, 2022, 1:59 PM +00:00

--- copy.c
+++ copy.c
@@ -4,15 +4,4 @@
 
 int main(void)
 {
-    // copy content from one variable to another
-    char *s = get_string("x: ");
-    char x = s[0];
-    char y = x;
-    printf("%c\n", x);
-    printf("%c\n\n", y);
-
-    // Make it upper to know if it copied correctly
-    y = toupper(x);
-    printf("%c\n", x);
-    printf("%c\n", y);
 }
