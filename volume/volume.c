f5c9bdda7005ffd29121aae6fe1f6ebfe89e9903
JaphethK <74491890+Japheth-prog@users.noreply.github.com>
2022-07-14 09:47:27 +0000

    Thu, Jul 14, 2022, 9:47 AM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -34,6 +34,12 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
+    for (int i = 0; i < HEADER_SIZE; i ++)
+    {
+        // Read
+        
+        // Write
+    }
 
     // TODO: Read samples from input file and write updated data to output file
 
