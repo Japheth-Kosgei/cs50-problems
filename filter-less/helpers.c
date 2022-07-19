0680978d1cb1b08839c6332b974a7de04c3a2f6c
JaphethK <74491890+Japheth-prog@users.noreply.github.com>
2022-07-19 11:40:59 +0000

    Tue, Jul 19, 2022, 11:40 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -3,6 +3,20 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i ++) // Rows
+    {
+        for (int j = 0; j < width; j ++) // Columns
+        {
+            // Calculate the average
+            double average_raw = round((((double)image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0));
+            int average = average_raw;
+
+            // Set the rgb values to average
+            image[i][j].rgbtBlue = average;
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtRed = average;
+        }
+    }
     return;
 }
 
