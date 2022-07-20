288d0f2a45b97acef83b6952adedd8375bbeeef3
JaphethK <74491890+Japheth-prog@users.noreply.github.com>
2022-07-20 12:11:04 +0000

    Wed, Jul 20, 2022, 12:11 PM +00:00

---
+++ average.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // Do some averages
+    int array1[] = {2, 1, 2};
+    int array2[] = {7, 6, 6};
+    int array3[] = {8, 5, 7};
+
+    int av_array1 = 
+}
