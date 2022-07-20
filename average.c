69d35a464a2b2f728736d457e9a6cd80d92fd59e
JaphethK <74491890+Japheth-prog@users.noreply.github.com>
2022-07-20 12:17:47 +0000

    Wed, Jul 20, 2022, 12:17 PM +00:00

--- average.c
+++ average.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+int average (int array);
+
 int main(void)
 {
     // Do some averages
@@ -10,4 +12,15 @@ int main(void)
     printf("Array1: %f\n", (array1[0] + array1[1] + array1[2]) / 3.0);
     printf("Array2: %f\n", (array2[0] + array2[1] + array2[2]) / 3.0);
     printf("Array3: %f\n", (array3[0] + array3[1] + array3[2]) / 3.0);
+}
+
+int average (int array)
+{
+    int len = sizeof(array) / sizeof(int);
+    int sum 0;
+
+    for (int i = 0; i < len; i ++)
+    {
+        sum += array[i];
+    }
 }
