8da320c97bc8ec8e49b496821da179965af1bcde
JaphethK <74491890+Japheth-prog@users.noreply.github.com>
2022-07-20 12:35:07 +0000

    Wed, Jul 20, 2022, 12:35 PM +00:00

--- average.c
+++ average.c
@@ -1,30 +1,28 @@
 #include <stdio.h>
 
-float average (int array[]);
+float average (float array[]);
 
 int main(void)
 {
     // Do some averages
-    int array1[] = {2, 1, 2};
-    int array2[] = {7, 6, 6};
-    int array3[] = {8, 5, 7};
+    float array1[] = {2, 1, 2};
+    float array2[] = {7, 6, 6};
+    float array3[] = {8, 5, 7};
 
-    printf("Array1: %f\n", average(array1));
+    printf("Array1: %3f\n", average(array1));
     printf("Array2: %f\n", average(array2));
     printf("Array3: %f\n", average(array3));
 }
 
-float average (int array[])
+float average (float array[])
 {
-    int len = sizeof(*array) / sizeof(int);
+    float len = sizeof(*array) / sizeof(int);
     float sum = 0;
-    int count = 0;
 
     for (int i = 0; i < len; i ++)
     {
         sum += array[i];
-        count ++;
     }
 
-    return (sum / count);
+    return (sum / len);
 }
