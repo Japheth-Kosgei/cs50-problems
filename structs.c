28c2b752aef35543f2127e37ab2462c3252d71aa
JaphethK <74491890+Japheth-prog@users.noreply.github.com>
2022-07-20 09:25:02 +0000

    Wed, Jul 20, 2022, 9:25 AM +00:00

---
+++ structs.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+typedef struct
+{
+    int red;
+    int green;
+    int blue;
+}
+rgb;
+
+int main(void)
+{
+    // Make a data structure similar to rgb and see how it copys
+    rgb array[2];
+
+    array[1].red = 
+}
