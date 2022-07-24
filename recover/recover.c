e288b7561a4fc29b22650597d5dfb07cbf37d23d
JaphethK <74491890+Japheth-prog@users.noreply.github.com>
2022-07-24 10:49:38 +0000

    Sun, Jul 24, 2022, 10:49 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
     {
         // Read 512-byte blocks of the data
         char *buffer = malloc(512);
-        fread(buffer, sizeof(*buffer), 1, mem_card);
+        fread(buffer, 512, 1, mem_card);
         if (*buffer == EOF)
         {
             break;
