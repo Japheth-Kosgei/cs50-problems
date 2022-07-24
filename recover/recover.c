b57e343be21b96704672896d61e0358f4743c548
JaphethK <74491890+Japheth-prog@users.noreply.github.com>
2022-07-24 10:35:55 +0000

    Sun, Jul 24, 2022, 10:35 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
     while (true)
     {
         // Read 512-byte blocks of the data
-        char buffer = malloc(sizeof(char))
+        char buffer = malloc(sizeof(char) * 64)
     }
 
     // Close the file
