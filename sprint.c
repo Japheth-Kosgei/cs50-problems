2ebc1fee7d8bdc25ff795ebec17529259df793b2
JaphethK <74491890+Japheth-prog@users.noreply.github.com>
2022-07-24 19:12:19 +0000

    Sun, Jul 24, 2022, 7:12 PM +00:00

--- sprint.c
+++ sprint.c
@@ -6,7 +6,7 @@ int main(void)
     // Make an series of strings from 000 to 999 and print
     int number_of_image = 0;
     char * filename = malloc(8);
-    filename = "000.jpg";
+    sprintf(filename, "%3i", 0);
     printf("%s\n", filename);
 
     for (int i = 1; i < 1000; i ++)
