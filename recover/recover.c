76b54985dc33437f041dd0beb7409a2983fe5b22
JaphethK <74491890+Japheth-prog@users.noreply.github.com>
2022-07-24 09:52:47 +0000

    Sun, Jul 24, 2022, 9:52 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+#include <stdbool.h>
+
 int main(int argc, char *argv[])
 {
- 
+    // Ensure only one command-line argument is given
+    if (argc != 2)
+    {
+        printf("Usage: recover <filename>\n");
+        return 1;
+    }
+
+    // Open the file
+    char * filename = argv[1];
+    FILE * file_ptr = fopen(filename, "r");
+
+    if (file_ptr == NULL)
+    {
+        printf("Error: File could not be opened.\n");
+        return 2;
+    }
+
+    //
+    char character;
+    while (true)
+    {
+        character = fgetc(file_ptr);
+        if (character == EOF)
+        {
+            printf("\n");
+            break;
+        }
+
+        printf("%c", character);
+    }
+
+    // Close the file
+    fclose(file_ptr);
 }
